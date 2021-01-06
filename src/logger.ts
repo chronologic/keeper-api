47daacdf295842a0e4e3e3e462f28fababb58998
Marcin <zolkiewski.marcin@gmail.com>
2021-01-06 18:58:54 +0100

    added postgres integration

--- src/logger.ts
+++ src/logger.ts
@@ -1,37 +1,31 @@
-import {
-  createLogger,
-  format,
-  transports
-} from 'winston';
+import { createLogger, format, transports } from "winston";
 
 const logTransports = [
   new transports.File({
-    level: 'error',
-    filename: './logs/error.log',
+    level: "error",
+    filename: "./logs/error.log",
     format: format.json({
       replacer: (key, value) => {
-        if (key === 'error') {
+        if (key === "error") {
           return {
             message: (value as Error).message,
-            stack: (value as Error).stack
+            stack: (value as Error).stack,
           };
         }
         return value;
-      }
-    })
+      },
+    }),
   }),
   new transports.Console({
-    level: 'debug',
-    format: format.prettyPrint()
-  })
+    level: "debug",
+    format: format.prettyPrint(),
+  }),
 ];
 
 const logger = createLogger({
-  format: format.combine(
-    format.timestamp()
-  ),
+  format: format.combine(format.timestamp()),
   transports: logTransports,
-  defaultMeta: { service: 'api' }
+  defaultMeta: { service: "api" },
 });
 
 export default logger;
