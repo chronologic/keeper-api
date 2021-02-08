36e73e49da281cfbe5e7b60de5df166148db2048
Marcin <zolkiewski.marcin@gmail.com>
2021-02-08 09:50:57 +0100

    updated env

--- src/env.ts
+++ src/env.ts
@@ -2,8 +2,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const LOG_LEVEL = process.env.LOG_LEVEL as string;
-
-export const NODE_ENV = process.env.NODE_ENV as string;
+export const LOG_LEVEL = (process.env.LOG_LEVEL as string) || 'info';
 
 export const PORT = Number(process.env.PORT || 3000);
